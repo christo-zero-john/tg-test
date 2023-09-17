aac3b37632a6e2fcd93fdd6f94b8773322000897
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2023-09-17 18:18:23 +0530

    commit 6 on 17 9 2023

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const {Telegraf} = require('telegraf');                                                    
+const {Telegraf} = require('telegraf');      
+const fs = require('fs');
+var user = 0;
 const bot = new Telegraf ('6571991794:AAFScohMfwF2y7_1wCuEGFbWf15yaXt6P_8');
-bot.command(["start"],(ctx, next)=>{
-    ctx.reply("Bot started!!");
+
+bot.use((ctx, next)=>{
+    
+    if(ctx.message.text != "/user"){
+        addUser(ctx);
+    }
 
     next(ctx);
+})
+
+
+bot.command(["user"],(ctx, next)=>{
+    ctx.reply("Add User\nSent me a user name ");
+    user = 1;
+    next(ctx);
 });
+
+function addUser(ctx){
+    var data = fs.readFileSync("data.json");
+    data = JSON.parse(data);
+    Total = data.total;
+    data.users[Total] = {
+        "uid":Total + 1,
+        "uname":ctx.message.text
+    }
+    Total++;
+    data.total = Total;
+    data = JSON.stringify(data);
+    fs.writeFileSync("data.json",data);
+}
+
+
+
+
 bot.launch();
